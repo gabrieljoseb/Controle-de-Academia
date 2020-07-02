07f0e5fa80dc2f6a8d212351fa1d1a98bce1114a
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-02 08:40:47 -0300

    separando as funcoes das rotas

---
+++ instructors.js
@@ -0,0 +1,13 @@
+// create
+exports.post = (req, res) => {
+
+    // pega todos os objetos em array do campo "name" do input, ex: ["avatar_url", "name", "birth"]
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "")
+            return res.send('please, fill all fields')
+    }
+
+    return res.send(req.body)
+}
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const routes = express.Router()
+const instructors = require('./instructors')
 
 routes.get("/", function (req, res) {
     return res.redirect('/instructors')
@@ -13,18 +14,7 @@ routes.get("/instructors/create", (req, res) => {
     return res.render('instructors/create')
 })
 
-routes.post("/instructors", (req, res) => {
-
-    // pega todos os objetos em array do campo "name" do input, ex: ["avatar_url", "name", "birth"]
-    const keys = Object.keys(req.body)
-
-    for (let key of keys) {
-        if (req.body[key] == "")
-            return res.send('please, fill all fields')
-    }
-
-    return res.send(req.body)
-})
+routes.post("/instructors", instructors.post)
 
 routes.get("/members", function (req, res) {
     return res.send('members')

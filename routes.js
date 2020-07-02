e61ee5665eef0c96f61ebd54539037b7c7cdb115
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-02 08:32:18 -0300

    validando dados no back end

--- routes.js
+++ routes.js
@@ -14,6 +14,15 @@ routes.get("/instructors/create", (req, res) => {
 })
 
 routes.post("/instructors", (req, res) => {
+
+    // pega todos os objetos em array do campo "name" do input, ex: ["avatar_url", "name", "birth"]
+    const keys = Object.keys(req.body)
+
+    for (let key of keys) {
+        if (req.body[key] == "")
+            return res.send('please, fill all fields')
+    }
+
     return res.send(req.body)
 })
 

5d39d409dd9042b3654d82337cae237d900111eb
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-08 09:50:13 -0300

    adicionando imagem de avatar na tabela

--- instructors.js
+++ instructors.js
@@ -95,7 +95,8 @@ exports.put = (req, res) => {
     const instructor = {
         ...foundInstructor,
         ...req.body,
-        birth: Date.parse(req.body.birth)
+        birth: Date.parse(req.body.birth),
+        id: Number(req.body.id)
     }
 
     data.instructors[id - 1] = instructor

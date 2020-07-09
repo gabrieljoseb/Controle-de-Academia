d0ab02cc4732c601ffc2fabf91e1248711bb8ee5
Gabriel Ribeiro <gjose.benevides@gmail.com>
2020-07-09 10:48:13 -0300

    criadas as rotas para members

--- routes.js
+++ routes.js
@@ -7,23 +7,23 @@ routes.get("/", function (req, res) {
 })
 
 routes.get("/instructors", instructors.index)
-
 routes.get("/instructors/create", (req, res) => {
     return res.render('instructors/create')
 })
-
 routes.get('/instructors/:id', instructors.show)
-
 routes.get('/instructors/:id/edit', instructors.edit)
-
 routes.post("/instructors", instructors.post)
-
 routes.put("/instructors", instructors.put)
-
 routes.delete("/instructors", instructors.delete)
 
-routes.get("/members", function (req, res) {
-    return res.send('members')
-})
 
+routes.get("/members", members.index)
+routes.get("/members/create", (req, res) => {
+    return res.render('members/create')
+})
+routes.get('/members/:id', members.show)
+routes.get('/members/:id/edit', members.edit)
+routes.post("/members", members.post)
+routes.put("/members", members.put)
+routes.delete("/members", members.delete)
 module.exports = routes
